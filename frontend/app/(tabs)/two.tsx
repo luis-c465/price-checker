ba767b5f6e7f5ba7cf8769de041d04d8fe2b905d
salvadorfrs5 <78862926+salvadorfrs5@users.noreply.github.com>
2024-03-22 19:16:21 -0400

    Added search bar in 2nd tab

--- frontend/app/(tabs)/two.tsx
+++ frontend/app/(tabs)/two.tsx
@@ -1,11 +1,25 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TextInput } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { useRouter } from 'expo-router';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
 export default function TabTwoScreen() {
+  const router = useRouter();
+
+  const handleSearchSubmit = () => {
+    router.push('/modal');
+  };
+  
   return (
     <View style={styles.container}>
+      <TextInput
+        style={{...styles.searchBar, color: 'white'}}
+        placeholder="Search..."
+        placeholderTextColor="white"
+        onSubmitEditing={handleSearchSubmit}
+      />
       <Text style={styles.title}>Tab Two</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/two.tsx" />
@@ -27,5 +41,12 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  searchBar: {
+    height: 40,
+    width: '80%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingLeft: 10,
   },
 });
