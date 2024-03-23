ec7054cb8d6a2870661878e7396ac01f6330f3d4
Luis Canada <canadaluis5@gmail.com>
2024-03-23 11:38:33 -0400

    Add production mode for backend

--- backend/main.py
+++ backend/main.py
@@ -7,16 +7,23 @@ from flask import Flask, jsonify, request
 
 from search import text_search_all
 
+DEBUG = True
+
 app = Flask(__name__)
 
 @app.route('/search')
 def users():
+	print('pain')
 	query = request.args.get("query")
 	if not query:
 		return "You must put a query!", 400
+
 	products = text_search_all(query)
 	serialized = [serialize_product(p) for p in products]
 	return jsonify(*serialized)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	if not DEBUG:
+		app.run(host="0.0.0.0", port=5001)
+	else:
+		app.run()
